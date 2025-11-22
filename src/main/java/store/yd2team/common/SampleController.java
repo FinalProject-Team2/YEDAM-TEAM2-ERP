904044223728f4e093b780b0cde8d7f0ed09a91a
minyounki <minyounki@gmail.com>
2025-11-22 21:19:41 +0900

    레이아웃 세팅

--- src/main/java/store/yd2team/common/SampleController.java
+++ src/main/java/store/yd2team/common/SampleController.java
@@ -1,5 +1,19 @@
 package store.yd2team.common;
 
-public class SampleController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class SampleController {
+	//test
+		@GetMapping("/")
+		public String selectall(Model model) {
+			
+			
+			
+			
+			model.addAttribute("test", "testone");
+			return "test";
+		}
 }
