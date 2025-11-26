41bab7d5d27266f948e6e561036a13abd19fdae3
O deng <oms9623@naver.com>
2025-11-26 17:20:54 +0900

    오민수/뭐했더라/25.11.26

--- src/main/java/store/yd2team/common/web/SubscriptionPlanController.java
+++ src/main/java/store/yd2team/common/web/SubscriptionPlanController.java
@@ -1,14 +1,20 @@
 package store.yd2team.common.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import store.yd2team.common.service.SubscriptionService;
 
 @Controller
 public class SubscriptionPlanController {
     
+	@Autowired
+	SubscriptionService subscriptionService;
+	
+	// 구독 플랜 페에지
     @GetMapping("/SubscriptionPlan")
     public String SubscriptionPlan() {
-        // templates/subscription/subscription.html 을 의미
         return "subscription/subscriptionPlan";
     }
     
