6c4fc1aadb308498907acab407cce4902e511178
yoonhee714 <cupid9710@gmail.com>
2025-11-26 17:00:10 +0900

    이윤희/영업-여신관리-화면구현/251126

---
+++ src/main/java/store/yd2team/business/web/CreditController.java
@@ -0,0 +1,17 @@
+package store.yd2team.business.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CreditController {
+
+	@GetMapping("/credit")
+	public String selectall(Model model) {
+
+		model.addAttribute("test", "testone");
+		return "business/credit";
+
+	}
+}
