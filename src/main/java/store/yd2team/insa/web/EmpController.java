28725c1a1e652095c3bc5fdfd226910a9fcd9d95
chikeket <minyounki@gmail.com>
2025-11-28 17:26:32 +0900

    myk-인사/사원페이지수정쿼리진행중-251128

--- src/main/java/store/yd2team/insa/web/EmpController.java
+++ src/main/java/store/yd2team/insa/web/EmpController.java
@@ -2,14 +2,20 @@ package store.yd2team.insa.web;
 
 
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import java.io.File;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import store.yd2team.insa.service.EmpService;
 import store.yd2team.insa.service.EmpVO;
@@ -25,7 +31,7 @@ public class EmpController {
 		
 		
 		
-		model.addAttribute("test", "testone");
+		//model.addAttribute("test", "testone");
 		return "insa/employee-register";
 
 	}
@@ -43,5 +49,38 @@ public class EmpController {
 		johoeKeyword.setClsf(clsf);		
 		
 		return empService.getListEmpJohoe(johoeKeyword);
+	}
+	
+	@Value("${uploadDir}")
+	private String uploadDir;
+	
+	@PostMapping("/empEdit")
+	@ResponseBody
+	public List<EmpVO> empRegist(@RequestPart("empVO") EmpVO empVO,
+	        @RequestPart(value = "photo", required = false) MultipartFile photo) throws IOException {
+		System.out.println("환경설정불러온값"+uploadDir);
+		// ✅ 파일 업로드 처리
+	    if (photo != null && !photo.isEmpty()) {
+	        String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/images/profil/";
+	        File dir = new File(uploadDir);
+	        if (!dir.exists()) dir.mkdirs();
+
+	        // ✅ 사번 기반 파일명 생성
+	        String fileName = empVO.getEmpId().toLowerCase() + ".jpg";
+	        File saveFile = new File(uploadDir, fileName);
+	        photo.transferTo(saveFile);
+
+	        // ✅ DB에 저장할 경로 세팅
+	        empVO.setProofPhoto("/images/profil/" + fileName);
+	    }
+
+	    // ✅ 사원 정보 DB 저장
+	    System.out.println("이엠프브이오 나와라"+empVO);	    
+	    empService.setDbEdit(empVO);
+	    EmpVO johoeKeyword = new EmpVO();
+		
+		
+		return empService.getListEmpJohoe(johoeKeyword);
+
 	}
 }
