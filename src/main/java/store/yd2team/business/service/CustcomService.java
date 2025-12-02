3b6d7e482af842dc6c1c7700c08a83649d0b21e5
yoonhee714 <cupid9710@gmail.com>
2025-12-02 17:22:24 +0900

    이윤희/고객사관리-신규등록모달작업중/251202

--- src/main/java/store/yd2team/business/service/CustcomService.java
+++ src/main/java/store/yd2team/business/service/CustcomService.java
@@ -4,5 +4,12 @@ import java.util.List;
 
 public interface CustcomService {
 
+	// 조회
 	List<CustcomVO> searchCustcom(CustcomVO vo);
+	
+	// 저장
+	int saveNewCust(CustcomVO vo) throws Exception;
+
+	//공통코드 조회
+	List<CustcomVO> getBSType();
 }
--- src/main/java/store/yd2team/business/service/impl/CustcomServiceImpl.java
+++ src/main/java/store/yd2team/business/service/impl/CustcomServiceImpl.java
@@ -15,8 +15,26 @@ public class CustcomServiceImpl implements CustcomService {
 
     private final CustcomMapper custcomMapper;
 
+    // 조회
     @Override
     public List<CustcomVO> searchCustcom(CustcomVO vo) {
         return custcomMapper.searchCustcom(vo);
     }
+    
+    // 공통코드 조회
+    @Override
+    public List<CustcomVO> getBSType() {
+    	return custcomMapper.selectBSType();
+    }
+    
+    
+    // 저장
+    @Override
+    public int saveNewCust(CustcomVO vo) throws Exception {
+        System.out.println("### Service saveNewCust 호출 ###");
+        int result = custcomMapper.insertCustcom(vo);
+        System.out.println("### result = " + result);
+        return 1;   // 변화 건수 상관없이 성공 처리
+    }
+
 }
