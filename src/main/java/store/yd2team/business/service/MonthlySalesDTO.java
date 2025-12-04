412654ae5153c05f9f4c17b8d5387804e25cf08a
heochangbeom <heoc4869@gmail.com>
2025-12-04 17:27:39 +0900

    허창범/20251203/휴먼이탈고객 조건별점수화기능

---
+++ src/main/java/store/yd2team/business/service/MonthlySalesDTO.java
@@ -0,0 +1,10 @@
+package store.yd2team.business.service;
+
+public class MonthlySalesDTO {
+    private String customerId;		//고객사
+    private int trendScore;              
+    private int recScore;
+    private int freqScore;
+    private int finalScore;
+    private int customerStatus;
+}
