a520ec9e7b6c74a17e518629642382283a3e236c
yoonhee714 <cupid9710@gmail.com>
2025-12-12 17:37:10 +0900

    이윤희/채권관리화면수정중/251212

--- src/main/java/store/yd2team/business/service/RciptService.java
+++ src/main/java/store/yd2team/business/service/RciptService.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 public interface RciptService {
 
+	//조회
 	List<RciptVO> searchRcipt(RciptVO searchVO);
 
-	List<RciptVO> searchCustcomId(String keyword);
-
-	List<RciptVO> searchCustcomName(String keyword);
+	//조회조건 자동완성
+	/*
+	 * List<RciptVO> searchCustcomId(String keyword); List<RciptVO>
+	 * searchCustcomName(String keyword);
+	 */
 
 	//입금내역
 	int insertRciptDetail(RciptVO vo);
--- src/main/java/store/yd2team/business/service/impl/RciptServiceImpl.java
+++ src/main/java/store/yd2team/business/service/impl/RciptServiceImpl.java
@@ -3,11 +3,13 @@ package store.yd2team.business.service.impl;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import store.yd2team.business.mapper.RciptMapper;
 import store.yd2team.business.service.RciptService;
 import store.yd2team.business.service.RciptVO;
+import store.yd2team.common.util.LoginSession;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class RciptServiceImpl implements RciptService {
     public List<RciptVO> searchRcipt(RciptVO searchVO) {
         return rciptMapper.selectRciptList(searchVO);
     }
+/*    
     // 조회 고객사 auto complete(고객코드, 고객사명)
     @Override
     public List<RciptVO> searchCustcomId(String keyword) {
@@ -29,9 +32,26 @@ public class RciptServiceImpl implements RciptService {
     public List<RciptVO> searchCustcomName(String keyword) {
         return rciptMapper.searchCustcomName(keyword);
     }
+*/
+    
     //입금내역
-	@Override
-	public int insertRciptDetail(RciptVO vo) {
-		return rciptMapper.insertRciptDetail(vo);
-	}
+    @Override
+    @Transactional
+    public int insertRciptDetail(RciptVO vo) {
+
+        vo.setVendId(LoginSession.getVendId());
+        vo.setCreaBy(LoginSession.getEmpId());
+        vo.setUpdtBy(LoginSession.getEmpId());
+
+        rciptMapper.callInsertRciptProcedure(vo);
+        
+        System.out.println(">>> RESULT_MSG FROM PROCEDURE = " + vo.getResultMsg());
+
+        // OUT 파라미터에 담긴 결과 체크
+        if (vo.getResultMsg() != null && vo.getResultMsg().startsWith("SUCCESS")) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
